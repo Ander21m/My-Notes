4d531df85b5b9799e533139bea369a7a9ae5e9fb
Ander21m <hinatashoyo871@gmail.com>
2023-09-13 23:09:36 +0700

    step-5

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -4,6 +4,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import 'dart:developer' as devtools show log;
 
+import 'package:mynotes/constants/routes.dart';
+
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -64,7 +66,7 @@ class _LoginViewState extends State<LoginView> {
                     password: password
                     );
                   
-                  Navigator.of(context).pushNamedAndRemoveUntil('/notes/', (route) => false,);
+                  Navigator.of(context).pushNamedAndRemoveUntil(notesRoute, (route) => false,);
                   }
                   on FirebaseAuthException catch(e){
                     if(e.code == 'user-not-found'){
@@ -80,7 +82,7 @@ class _LoginViewState extends State<LoginView> {
                 
               ),
               TextButton(onPressed:() {
-                Navigator.of(context).pushNamedAndRemoveUntil('/register/', (route) => false);
+                Navigator.of(context).pushNamedAndRemoveUntil(registerRoute, (route) => false);
               },
               child:const Text("Not Registered yet?Register Here!"),
               
